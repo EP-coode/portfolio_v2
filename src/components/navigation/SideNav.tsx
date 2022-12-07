730cd544c98956dc47949011f0a8940f8f2e1890
Ernest Przybył <ernestprzybyl@gmail.com>
2022-12-07 20:31:32 +0100

    logo to webp

--- src/components/navigation/SideNav.tsx
+++ src/components/navigation/SideNav.tsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 import Image from "next/image";
 import React from "react";
 
-import logo from "../../../public/images/logo.png";
+import logo from "../../../public/images/logo.webp";
 import { NavProps } from ".";
 import SideNavAction from "./SideNavAction";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
