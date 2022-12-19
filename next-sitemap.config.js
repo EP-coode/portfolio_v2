194c0778d8e21b31679052c2c37f07b71e9d0d3c
Ernest Przybył <ernestprzybyl@gmail.com>
2022-12-19 16:10:04 +0100

    disable sitemap splitting

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: process.env.WEBSITE_URL || "https://example.com",
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
   changefreq: "daily",
   priority: 1.0,
 };
